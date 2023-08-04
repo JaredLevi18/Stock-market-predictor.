d333de6293ba09499e206882c2c9122f0e5389b4
Jared Levi De La Fuente Rodriguez <djaredlevi@gmail.com>
2023-08-04 08:00:04 -0600

    Create stock_price.py

---
+++ parts of the program/stock_price.py
@@ -0,0 +1,16 @@
+from langchain.agents import AgentType, initialize_agent
+from langchain.tools import Tool
+from langchain.tools import DuckDuckGoSearchRun
+
+search_stock = DuckDuckGoSearchRun()
+
+tools = [
+    Tool.from_function(
+        func=search_stock.run,
+        name="Search Stock price",
+        description="Useful for when you need to know a stock's price on the internet",
+    ),
+]
+
+def get_stock_price(command):
+    x = 10
