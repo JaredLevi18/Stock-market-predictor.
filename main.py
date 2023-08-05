4eb0171b7fcda0dc2b55c371165bf30f91bd36d1
Jared Levi De La Fuente Rodriguez <jk20042019@gmail.com>
2023-08-05 08:33:58 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from parts_of_the_program import tools
+from langchain.agents import AgentType, initialize_agent
+from langchain.chat_models import ChatOpenAI
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+llm = ChatOpenAI(temperature=0.2)
+
+agent = initialize_agent(
+    tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
+
+command = input("Enter the name of the company you'd like to know its stock price: ")
+agent.run(command)
